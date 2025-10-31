f1fa62e0a6090fe1d8f3fc5a9e8c89ef7356df50
DominicDev <s0l0estudi4r@gmail.com>
2025-10-31 12:38:35 +0100

    v0.1.1

--- src/components/ContainerComponents.tsx
+++ src/components/ContainerComponents.tsx
@@ -1,10 +1,10 @@
-import { projects } from '@/App';
+import { pathsAndImages } from '@/App';
 import '@/styles/ContainerComponents.css';
 import DomeGallery from './dome-gallery/DomeGallery';
 import { withBase } from '@/utils/functionsTs';
 
 export const ContainerComponents = () => {
-  const arrayOfImages = projects.map(obj => ({
+  const arrayOfImages = pathsAndImages.map(obj => ({
     src: withBase(obj.imageUrl),
     web: obj.path
   }));
