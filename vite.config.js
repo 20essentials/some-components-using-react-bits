7713cc0b60012702196dc5b22bfcb10b88dc959d
DominicDev <s0l0estudi4r@gmail.com>
2025-11-10 12:49:01 +0100

    v0.8.1

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+// https://vite.dev/config/
+export default defineConfig({
+    //Localhost
+    // base: './',
+    //Deploy
+    base: '/some-components-using-react-bits/',
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        }
+    }
+});
